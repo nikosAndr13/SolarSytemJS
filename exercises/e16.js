aae7906106d8ee4dc0960920b056f9db39da483c
Nikos Andriopoulos <nickos2014andriopoulos@gmail.com>
2022-12-30 19:33:05 +0200

    Finish All Exercises

--- exercises/e16.js
+++ exercises/e16.js
@@ -1,4 +1,5 @@
-import { data } from "../data/data";
+// see e16.md
+
 
 // SPACE DATA EXERCISE 16
 // Return the year with the greatest number of Asteroids discoveries
@@ -29,3 +30,6 @@ export function getGreatestDiscoveryYear(data) {
 // Once you're finished run the test with "npm run test-16"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
+
+export function find(array, callback) {}
+
